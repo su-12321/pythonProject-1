646a2ff19b613d5b479da9c51961b33580ae064f
su-12321 <13937197833@163.com>
2026-01-01 03:48:49 +0800

    Add files via upload

---
+++ myblog/urls.py
@@ -0,0 +1,17 @@
+"""
+URL configuration for myblog project.
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),  # 包含博客应用的URL
+]
+
+# 开发环境下的静态文件和媒体文件服务
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ myblog/wsgi.py
@@ -0,0 +1,11 @@
+"""
+WSGI config for myblog project.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myblog.settings')
+
+application = get_wsgi_application()
